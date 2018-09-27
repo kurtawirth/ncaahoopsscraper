682c8800c9a76d3e07f257f16ad4e5315ad753d6
Kurt Wirth <kurtawirth@gmail.com>
2018-09-27 10:38:12 -0400

    Include library(rvest) and eliminate bad code

--- scrape.R
+++ scrape.R
@@ -1,3 +1,5 @@
+library(rvest)
+
 startpage <- read_html("https://www.sports-reference.com/cbb/boxscores/index.cgi?month=4&day=2&year=2018")
 
 session <- html_session("https://www.sports-reference.com/cbb/boxscores/index.cgi?month=4&day=2&year=2018")
@@ -5,7 +7,3 @@ session <- html_session("https://www.sports-reference.com/cbb/boxscores/index.cg
 WinningTeamName <- startpage %>% html_nodes(".winner a") %>% html_text()
 
 LosingTeamName <- startpage %>% html_nodes(".loser td:nth-child(1) a") %>% html_text()
-
-HomeTeamName <- session %>% follow_link(css = ".gamelink a") %>% html_nodes("#line-score .thead~ tr+ tr a") %>% html_text()
-
-VisitorTeamName <- session %>% follow_link(css = ".gamelink a") %>% html_nodes("#line-score .thead~ tr+ tr a") %>% html_text()
