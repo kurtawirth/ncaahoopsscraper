d3a340c8ef5887d08ac8d1af13aecafdb8313414
Kurt Wirth <35779108+kurtawirth@users.noreply.github.com>
2018-10-17 16:50:44 -0400

    Set New Column Names

--- R/test.R
+++ R/test.R
@@ -131,3 +131,5 @@ dat <- function(x){
 }
 
 out <- select(out, "V2", "V4", "V5", "V6", "V8", "V10", "V12", "V14", "V16", "V18", "V20")
+                 
+colnames(out) <- c("TeamName", "W-L", "AdjEM", "AdjO", "AdjD", "AdjT", "Luck", "SOSAdjEM", "SOSOppO", "SOSOppD", "NCSOSAdjEM")
