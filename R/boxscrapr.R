1a60e2972e56f8e61be4dbf107e0eaacfb2cad80
Kurt Wirth <kurtawirth@gmail.com>
2019-06-29 14:48:33 -0400

    Add Boxscrapr
    Adds non-working WIP file for boxscrapr

---
+++ R/boxscrapr.R
@@ -0,0 +1,93 @@
+#' boxscrapr
+#'
+#' Scrapes box scores for any season of college basketball.
+#'
+#' Takes inputs for beginning and end of season and scrapes www.sports-reference.com for all box scores between those dates (inclusive).
+#'
+#' @author Kurt Wirth
+#'
+#' @param x The beginning date of data scraping, in the format (TEST)
+#' "yyyy/mm/dd".
+#'
+#' @param y The ending date of data scraping, in the format
+#' "yyyy/mm/dd".
+#'
+#' @return A tidy dataframe with the following variables for all games within
+#' the date range: XXX
+#'
+#' @examples
+#' \dontrun{boxscrapr("2017/11/10", "2018/04/02")}
+#' @export
+
+library(rvest)
+
+library(dplyr)
+
+boxscrapr <- function(x, y) {
+  
+  StartDate <- x
+  
+  EndDate <- y
+  
+  dates <- seq(as.Date(StartDate), as.Date(EndDate), "day") %>% format("%m-%d-%Y")
+  
+  df <- data.frame()
+  
+  ##Create loop for cycling through pages
+  
+  for(date in dates){
+    tryCatch({
+      split <- unlist(strsplit(date, "-"))
+      month <- split[1]
+      day <- split[2]
+      year <- split[3]
+      Page <- paste("https://www.sports-reference.com/cbb/boxscores/index.cgi?month=",month,"&day=",day,"&year=",year, sep="")
+      
+      ReadPage <- read_html(Page)
+      
+      NumGames <- length(ReadPage %>% rvest::html_nodes(".teams"))
+      
+      for(game in 1:NumGames){
+        
+        tryCatch({
+        
+            HomeTeamName <- ReadPage %>% html_nodes(paste(".nohover:nth-child(", game + 1, ") tr:nth-child(2) td:nth-child(1) a")) %>% html_text()
+            
+            HomeTeamName <- as.character(HomeTeamName)
+            
+            AwayTeamName <- ReadPage %>% html_nodes(paste(".nohover:nth-child(", game + 1, ") tr:nth-child(1) td:nth-child(1) a")) %>% html_text()
+            
+            AwayTeamName <- as.character(AwayTeamName)
+            
+            HomeTeamScore <- ReadPage %>% html_nodes(paste(".nohover:nth-child(", game + 1, ") tr:nth-child(2) td:nth-child(2)")) %>% html_text()
+            
+            HomeTeamScore <- as.numeric(HomeTeamScore)
+            
+            AwayTeamScore <- ReadPage %>% html_nodes(paste(".nohover:nth-child(", game + 1, ") tr:nth-child(1) td:nth-child(2)")) %>% html_text()
+            
+            AwayTeamScore <- as.numeric(AwayTeamScore)
+            
+            GameDate <- ReadPage %>% html_nodes(".current strong") %>% html_text()
+            
+            GameDate <- as.character(GameDate)
+            
+            Session <- rvest::html_session(Page)
+            
+            rvest::follow_link(Session, "Final")
+            
+            AwayTeam_FGM <- ReadPage %>% rvest::html_nodes("#box-score-basic-coppin-state .thead td:nth-child(3)") %>% html_text()
+            
+        tmp_df <- data.frame(VARIABLES)
+        
+        tmp_df <- tmp_df %>%
+          dplyr::mutate_if(is.factor, as.character)
+        
+        df <- dplyr::bind_rows(df, tmp_df)}, error = function(e) print(e))
+        
+        assign("scores", df, envir=globalenv())
+        
+      }}, error = function(e) print(e))
+    
+  }
+  
+}
