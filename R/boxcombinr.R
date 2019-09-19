f822839b5dc47d4e1c4b271b000c4dd70546c4c9
Kurt Wirth <kurtawirth@gmail.com>
2019-09-18 22:12:03 -0400

    Add boxcombinr
    Boxcombinr combines all box scores since 2010 as well as collapsing games into one row.

--- R/boxcombinr.R
+++ R/boxcombinr.R
@@ -12,6 +12,16 @@ bsdata[[6]] = data.table::fread("~/Programming/ncaahoopsscraper/data/BoxScores/2
 bsdata[[7]] = data.table::fread("~/Programming/ncaahoopsscraper/data/BoxScores/201617.csv", header = T, sep = ',') %>% as.data.frame()
 bsdata[[8]] = data.table::fread("~/Programming/ncaahoopsscraper/data/BoxScores/201718.csv", header = T, sep = ',') %>% as.data.frame()
 bsdata[[9]] = data.table::fread("~/Programming/ncaahoopsscraper/data/BoxScores/201819.csv", header = T, sep = ',') %>% as.data.frame()
+
 boxscores_total = bind_rows(bsdata) %>% select(-V1) %>% mutate(Date = as.Date(Date, "%B %d, %Y")) %>%
   rename(PCT_3 = "3PT%", PCT_FG = "FG%", PCT_FT = "FT%")
+
+boxscore_home = boxscores_total %>% filter(Role == "Home") %>% select(-Role) %>% rename(AwayTeamName = "Opponent")
+colnames(boxscore_home)[c(-1, -3)] = paste0("Home",colnames(boxscore_home)[c(-1, -3)])
+
+boxscore_away = boxscores_total %>% filter(Role == "Away") %>% select(-Role, -Opponent) %>% rename(AwayTeamName = "TeamName")
+colnames(boxscore_away)[c(-1, -2)] = paste0("Away",colnames(boxscore_away)[c(-1, -2)])
+
+boxscores_total = boxscore_home %>% inner_join(boxscore_away, by = c("Date", "AwayTeamName"))
+
 save(boxscores_total, file = "boxscores_total.RDA")
