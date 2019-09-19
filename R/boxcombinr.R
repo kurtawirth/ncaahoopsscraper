53b384d8b8aad95c7a56da0fdfab3b51ad461040
Kurt Wirth <kurtawirth@gmail.com>
2019-09-18 23:31:41 -0400

    Updating from 9/18/19
    Updates boxcombinr and adds data.

--- R/boxcombinr.R
+++ R/boxcombinr.R
@@ -1,8 +1,11 @@
-setwd("")
+setwd("~/Programming/ncaahoopsscraper/data/BoxScores")
 
 bsdata = list()
+
 require(data.table)
+
 library(dplyr)
+
 bsdata[[1]] = data.table::fread("~/Programming/ncaahoopsscraper/data/BoxScores/201011.csv", header = T, sep = ',') %>% as.data.frame()
 bsdata[[2]] = data.table::fread("~/Programming/ncaahoopsscraper/data/BoxScores/201112.csv", header = T, sep = ',') %>% as.data.frame()
 bsdata[[3]] = data.table::fread("~/Programming/ncaahoopsscraper/data/BoxScores/201213.csv", header = T, sep = ',') %>% as.data.frame()
@@ -16,12 +19,18 @@ bsdata[[9]] = data.table::fread("~/Programming/ncaahoopsscraper/data/BoxScores/2
 boxscores_total = bind_rows(bsdata) %>% select(-V1) %>% mutate(Date = as.Date(Date, "%B %d, %Y")) %>%
   rename(PCT_3 = "3PT%", PCT_FG = "FG%", PCT_FT = "FT%")
 
-boxscore_home = boxscores_total %>% filter(Role == "Home") %>% select(-Role) %>% rename(AwayTeamName = "Opponent")
-colnames(boxscore_home)[c(-1, -3)] = paste0("Home",colnames(boxscore_home)[c(-1, -3)])
+load("~/Programming/project/data/notd1teams.RDA")
 
-boxscore_away = boxscores_total %>% filter(Role == "Away") %>% select(-Role, -Opponent) %>% rename(AwayTeamName = "TeamName")
-colnames(boxscore_away)[c(-1, -2)] = paste0("Away",colnames(boxscore_away)[c(-1, -2)])
+boxscore_home = boxscores_total %>% filter(Role == "Home") %>% select(-Role) %>% rename(AwayTeamName = "Opponent") %>% filter(!(AwayTeamName %in% notd1teams)) %>%
+  rename(HomeTeamName = "TeamName") %>% filter(!(HomeTeamName %in% notd1teams))
 
-boxscores_total = boxscore_home %>% inner_join(boxscore_away, by = c("Date", "AwayTeamName"))
+colnames(boxscore_home)[c(-1, -2, -3)] = paste0("Home",colnames(boxscore_home)[c(-1, -2, -3)]) 
+
+boxscore_away = boxscores_total %>% filter(Role == "Away") %>% select(-Role) %>% rename(AwayTeamName = "TeamName") %>% rename(HomeTeamName = "Opponent") %>%
+  filter(!(AwayTeamName %in% notd1teams)) %>% filter(!(HomeTeamName %in% notd1teams))
+
+colnames(boxscore_away)[c(-1, -2, -3)] = paste0("Away",colnames(boxscore_away)[c(-1, -2, -3)])
+
+boxscores_total = boxscore_home %>% inner_join(boxscore_away, by = c("Date", "AwayTeamName", "HomeTeamName")) 
 
 save(boxscores_total, file = "boxscores_total.RDA")
